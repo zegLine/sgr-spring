41d517d009c65353b391e99b3f24285f151ebdd3
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-12-08 14:12:17 +0100

    initial commit

---
+++ src/main/java/com/zegline/sgrspring/SgrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zegline.sgrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgrSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zegline/sgrspring/SgrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zegline.sgrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
