6850f50a8b6ef0a0f84c2b7748cdbebda7fc73e5
Eugen Zdrincu <eugenzdrincu@gmail.com>
2023-12-10 11:27:52 +0100

    completed item service test

--- src/test/java/com/zegline/sgrspring/service/business/SGRItemServiceTests.java
+++ src/test/java/com/zegline/sgrspring/service/business/SGRItemServiceTests.java
@@ -11,10 +11,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class SGRItemServiceTests {
@@ -28,18 +28,42 @@ public class SGRItemServiceTests {
     @Test
     @Transactional
     public void testCreateItem() {
+        // Test Data
         String name = "test item";
         double weight = 1.2;
 
+        // Create an item and get its generated ID
         SGRItem created = is.createItem(name, weight);
         String created_id = created.getId();
 
+        // The name should match the test data
         assertEquals(created.getItem_name(), name);
+        // Find it in the repo
         Optional<SGRItem> opt_found = ir.findById(created_id);
         if (opt_found.isEmpty()) fail("SGRItem not found in Repo after created using Service");
 
+        // Hopefully it is the same one as the one we created
         SGRItem found = opt_found.get();
         assertEquals(found, created);
+    }
+
+    @Test
+    @Transactional
+    public void testGetAllItems() {
+        // Insert two items into the repo
+        SGRItem created1 = is.createItem("created1", 0.0);
+        SGRItem created2 = is.createItem("created2", 0.0);
+
+        // Query using Service getItems function
+        List<SGRItem> queried = is.getItems();
+
+        // Hopefully, the items should be returned
+        assertTrue(queried.contains(created1));
+        assertTrue(queried.contains(created2));
+
+        // The queried should ONLY contain the two elements
+        // since the DB should be clear before the test
+        assertEquals(queried.size(), 2);
     }
 
 }
