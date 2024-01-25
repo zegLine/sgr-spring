bb9f30639d9bafdc0a65f13b928eee0d083f53ac
Eugen Zdrincu <eugenzdrincu@gmail.com>
2024-01-25 16:55:47 +0100

    added securityconfig with jwt

--- src/main/java/com/zegline/sgrspring/config/security/SecurityConfig.java
+++ src/main/java/com/zegline/sgrspring/config/security/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.zegline.sgrspring.config.security;
 
+import com.zegline.sgrspring.filter.security.JwtAuthenticationFilter;
+import com.zegline.sgrspring.service.security.SGRUserService;
+import com.zegline.sgrspring.utils.security.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authConfiguration;
+
+    @Autowired
+    private SGRUserService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,15 +37,23 @@ public class SecurityConfig {
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .httpBasic(Customizer.withDefaults())
                 .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(userDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authConfiguration.getAuthenticationManager();
     }
 
 }
