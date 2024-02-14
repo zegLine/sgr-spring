2f2a2b35e8503814d9954f9572fc3ec7c4cf4742
Eugen Zdrincu <eugen.zdrincu@deutschepost.de>
2024-02-14 09:26:36 +0100

    added delete route for pg

--- src/main/java/com/zegline/sgrspring/controller/business/SGRPretGarantieController.java
+++ src/main/java/com/zegline/sgrspring/controller/business/SGRPretGarantieController.java
@@ -3,9 +3,12 @@ package com.zegline.sgrspring.controller.business;
 import com.zegline.sgrspring.model.business.SGRPretGarantie;
 import com.zegline.sgrspring.repository.business.SGRPretGarantieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/garantie/pret")
@@ -30,6 +33,19 @@ public class SGRPretGarantieController {
     public List<SGRPretGarantie> getAllPretGarantieItems() {
         // Retrieve all "pret garantie" items from the repository
         return (List<SGRPretGarantie>) pgr.findAll();
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Long> deletePretGarantie(@PathVariable Long id) {
+        Optional<SGRPretGarantie> pretGarantieToDeleteOpt = pgr.findById(id);
+
+        if (pretGarantieToDeleteOpt.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        pgr.deleteById(id);
+
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
     @GetMapping("/curent")
